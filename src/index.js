4626e686d0c0e9fb8479ea5ef707add537e539e1
Matias Ozuna <matiasozuna@gmail.com>
2022-09-06 19:42:44 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './routes/App';
+
+// Esto aplicaba a la version vieja de react:
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+// Con las actualizaciones quedaria asi: 
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<Apptab= "home" />);
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/notfound.scss';
+
+const NotFound = () => {
+    return (
+    <div className="notfounderror">
+        <h1>ERROR 404.</h1>
+        <h2>NOT FOUND</h2>
+    </div>
+    );
+}
+
+export default NotFound;
